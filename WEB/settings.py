139acdee6ad7968ff2e450170911513fc76a5e28
DmBilyk <dmbilyk3861@gmail.com>
2024-03-09 11:45:36 +0200

    Azure google auth

--- WEB/settings.py
+++ WEB/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-uy+4)+m@8mh+qck@&v!yr)8%lls6n=#wi4+5(7)c87dsp02-j$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['calm-connections.azurewebsites.net']
+ALLOWED_HOSTS = ['calm-connections.azurewebsites.net', '127.0.0.1']
+
 
 
 # Application definition
