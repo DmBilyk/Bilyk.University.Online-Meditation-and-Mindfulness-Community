8079f8a01c5e9030bd1adc3ee7344bdfe5ac00e6
DmBilyk <dmbilyk3861@gmail.com>
2024-03-09 10:45:55 +0200

    deplyment.py create

--- WEB/wsgi.py
+++ WEB/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+
+settings_module = 'WEB.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'WEB.settings'
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WEB.settings')
 
 application = get_wsgi_application()
