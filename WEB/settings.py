cebd1a7b026c4f5d99561232e3912235e8a3b2f0
DmBilyk <dmbilyk3861@gmail.com>
2024-03-20 15:44:14 +0200

    settings.py

--- WEB/settings.py
+++ WEB/settings.py
@@ -158,7 +158,7 @@ LOGOUT_REDIRECT_URL = '/'
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = [BASE_DIR, 'static']
+# STATIC_ROOT = [BASE_DIR, 'static']
 
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [STATIC_DIR]
