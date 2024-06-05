0f78a25dd4a5b75ca889f5a778e5423c2cdd51b5
DmBilyk <dmbilyk3861@gmail.com>
2024-06-05 14:39:56 +0300

    fix

--- WEB/settings.py
+++ WEB/settings.py
@@ -232,24 +232,6 @@ STATICFILES_DIRS = [STATIC_DIR]
 
 
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': '/var/log/django/debug.log',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
 
 
 ###configuration for media file storing and reriving media file from gcloud
